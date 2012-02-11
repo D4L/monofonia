0fe43a9ef81324fd08520716e16768557a46e51d
Austin Lee <austinl@quicinc.com>
2012-02-10 16:04:30 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monofonia::Application.config.secret_token = 'd4ada438563f2f4b2fdec542470dcf319b3c3a69f9aa4de37bdf5f98aee7d6f337bb1729665a05315451d67627f0d73f13861163f6d5a412a3d24df5b60d65da'
